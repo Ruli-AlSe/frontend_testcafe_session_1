32e61c8e8b16b854fc9763fde5910e99e7699073
Raul Almanza <ra.almanzaserrano@gmail.com>
2021-03-15 14:48:08 -0600

    Intall and configure dotenv to use environmental variables

--- page_model/data/Constants.js
+++ page_model/data/Constants.js
@@ -1,7 +1,9 @@
+const dotenv = require('dotenv').config();
+
 export const CREDENTIALS = {
     VALID_USER: {
-        USERNAME: 'standard_user',
-        PASSWORD: 'secret_sauce'
+        USERNAME: dotenv.parsed.USERNAME,
+        PASSWORD: dotenv.parsed.PASSWORD
     },
     INVALID_USER: {
         USERNAME: 'invalid_username',
